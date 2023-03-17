aca82b3ac112ae1c1c8b327784cc6ecf0ffaabe4
Umit Tas <umittas2000@gmail.com>
2023-03-17 09:57:45 -0300

    Updated after conflict

--- KDFTest1/FetchData.cs
+++ KDFTest1/FetchData.cs
@@ -30,36 +30,5 @@ namespace KDFTest1
                 Console.WriteLine(ex.Message); // Catch any exceptions that occur and display the error message
             }
         }
-
-        //public void LoginWithValidCredentials()
-        //{
-        //    try
-        //    {
-        //        // var lines = loadData.Read(Environment.GetEnvironmentVariable("PATH_DATA_SOURCE"));
-        //        var lines = loadData.Read(Common.Basepath());
-
-        //        loadData.ExecuteTestCases(lines);
-        //    }catch(Exception ex) { 
-        //        Console.WriteLine(ex.Message);  
-        //    }
-        //}
-
-
-
-
-        //public void LoginWithInvalidCredentials()
-        //{
-        //    try
-        //    {
-        //        //var lines = loadData.Read(Environment.GetEnvironmentVariable("PATH_DATA_SOURCE_INVALID_TEST"));
-        //        var lines = loadData.Read(Common.Basepath());
-
-        //        loadData.ExecuteTestCases(lines);
-        //    }catch(Exception ex)
-        //    {
-        //        Console.WriteLine(ex.Message);
-
-        //    }
-        //}
     }
 }
